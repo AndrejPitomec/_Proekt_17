885ea9fc71f51fcb30909c64601baeec0f4c6edf
AndrejPitomec <148689748+AndrejPitomec@users.noreply.github.com>
2023-12-22 09:36:04 +0100

    Add files via upload

---
+++ Project_16/src/sturent.java
@@ -0,0 +1,27 @@
+
+ class sturent {
+
+
+	int id;
+	String ime;
+	int starotst;
+	
+	Student(int i, String n){
+	id=i;
+	ime=n;
+	
+}
+ Studnet (int i, String n,  int a)
+ id=i;
+ ime=n;
+ student=a;
+ }
+ 
+    void display();
+   System.out.println(id+""+ime+""+starost);
+   	public static void main(String args[]) {
+	 Student s1 =new Student(111,"Petar");
+	 Student s2= new Student(222,"Marko",25);
+	 s1.display();
+	 s2.display();
+ }
